8754f7532c9faa2e35fad87c1b37acf1367ef71c
Aakash N S <aakashns.sky@gmail.com>
2020-01-23 00:15:31 +0530

    a commit from jovian

--- jovian-v2-script.py
+++ jovian-v2-script.py
@@ -1,4 +1,6 @@
 import jovian
 
-jovian.commit(project='jovian-v2-script-test2', message='Testing message from script', privacy='secret');
+jovian.commit(project='jovian-v2-script-test2', 
+              message='Testing message from script', 
+              privacy='secret', git_message='a commit from jovian')
 
